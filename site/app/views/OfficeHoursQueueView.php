6ac11424700c9e3e28ccbf8309c381eb22c2056c
Eli Schiff <eli@elischiff.org>
2019-11-07 21:18:25 -0500

    added stats to student pages

--- site/app/views/OfficeHoursQueueView.php
+++ site/app/views/OfficeHoursQueueView.php
@@ -5,8 +5,10 @@ namespace app\views;
 use app\models\User;
 use app\models\OfficeHoursQueueStudent;
 use app\models\OfficeHoursQueueInstructor;
+use app\libraries\DateUtils;
 
 class OfficeHoursQueueView extends AbstractView {
+
     public function showQueueStudent($oh_queue) {
         $queue_stats = $this->core->getQueries()->getQueueStats();
 
@@ -17,14 +19,16 @@ class OfficeHoursQueueView extends AbstractView {
         'remove_url' => $this->core->buildCourseUrl(["office_hours_queue/remove"]),
         'oh_queue' => $oh_queue,
         'queue_open' => $this->core->getQueries()->isQueueOpen(),
-        'avg_wait_time' => $queue_stats['avg_wait_time'],
-        'avg_help_time' => $queue_stats['avg_help_time'],
+        'avg_wait_time' => $this->intervalToString($queue_stats['avg_wait_time']),
+        'avg_help_time' => $this->intervalToString($queue_stats['avg_help_time']),
         'unique_students' => $queue_stats['unique_students'],
         'total_helped' => $queue_stats['total_helped']
         ]);
     }
 
     public function showQueueInstructor($oh_queue) {
+        $queue_stats = $this->core->getQueries()->getQueueStats();
+
         $this->core->getOutput()->addBreadcrumb("Office Hours Queue");
         $this->core->getOutput()->renderTwigOutput("OfficeHoursQueueInstructor.twig", [
         'csrf_token' => $this->core->getCsrfToken(),
@@ -38,7 +42,17 @@ class OfficeHoursQueueView extends AbstractView {
         'empty_queue_url' => $this->core->buildCourseUrl(["office_hours_queue/empty"]),
         'remove_url' => $this->core->buildCourseUrl(["office_hours_queue/remove"]),
         'start_help_url' => $this->core->buildCourseUrl(["office_hours_queue/startHelp"]),
-        'finish_help_url' => $this->core->buildCourseUrl(["office_hours_queue/finishHelp"])
+        'finish_help_url' => $this->core->buildCourseUrl(["office_hours_queue/finishHelp"]),
+        'avg_wait_time' => $this->intervalToString($queue_stats['avg_wait_time']),
+        'avg_help_time' => $this->intervalToString($queue_stats['avg_help_time']),
+        'unique_students' => $queue_stats['unique_students'],
+        'total_helped' => $queue_stats['total_helped']
         ]);
+    }
+
+    private function intervalToString($interval){
+        list($time, $ms) = preg_split("/\./", $interval);
+        list($hour, $min, $sec) = preg_split("/:/", $time);
+        return $hour."h ".$min."m ".$sec."s ";
     }
 }
