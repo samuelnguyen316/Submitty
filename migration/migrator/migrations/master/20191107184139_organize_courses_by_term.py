5c266bd50f1704aa727861d93be85af5aefd5c5b
pbailie <pbailie@users.noreply.github.com>
2019-11-08 21:12:06 -0500

    Changes to be committed: modified:   migration/migrator/migrations/master/20191107184139_organize_courses_by_term.py
    WIP

--- migration/migrator/migrations/master/20191107184139_organize_courses_by_term.py
+++ migration/migrator/migrations/master/20191107184139_organize_courses_by_term.py
@@ -1,6 +1,7 @@
 """Migration for the Submitty master database."""
 
 import re
+from datetime import datetime
 from pprint import pprint
 
 def up(config, database):
@@ -12,6 +13,28 @@ def up(config, database):
     :param database: Object for interacting with given database for environment
     :type database: migrator.db.Database
     """
+    def enter_and_validate_date(suggested_date, msg):
+        """
+        Validate user entered date string.
+
+        :param suggested_date: date string suggested to user.
+        :type suggested_date: String
+        :param msg: message to user asking for date.
+        :type msg: String
+        :return: validated date recieved from user
+        :rtype: String
+        """
+        while True:
+            date = input(msg)
+            if date == "":
+                date = suggested_date[1:-1]
+            # Validate date entry.
+            try:
+                datetime.strptime(date, "%m/%d/%Y")
+                return date
+            except:
+                print(f"Invalid date: '{date}'\n")
+    # END function enter_and_validate_date()
 
     # Create terms table
     database.execute("""
@@ -28,7 +51,7 @@ CREATE TABLE IF NOT EXISTS terms (
     database.execute("SELECT DISTINCT semester FROM courses ORDER BY semester ASC;")
     term_codes = database.fetchall()
 
-    # INSERT term codes into new terms table.
+    # We need information for each term code that already exists.
     # 1. ask sysadmin for name for each term.
     #   a. suggest that sXX = "Spring 20XX".
     #   b. suggest that fXX = "Fall 20XX".
@@ -44,27 +67,43 @@ CREATE TABLE IF NOT EXISTS terms (
         if re.fullmatch("^[fsu]\d{2}$", code):
             if code[0:1] == "f":
                 suggested_name = "Fall 20" + code[1:3]
-                suggested_start_date = "09/01/" + code[1:3]
-                suggested_end_date = "12/23/" + code[1:3]
+                suggested_start_date = "09/01/20" + code[1:3]
+                suggested_end_date = "12/23/20" + code[1:3]
             elif code[0:1] == "s":
                 suggested_name = "Spring 20" + code[1:3]
-                suggested_start_date = "01/02/" + code[1:3]
-                suggested_end_date = "05/30/" + code[1:3]
+                suggested_start_date = "01/02/20" + code[1:3]
+                suggested_end_date = "05/30/20" + code[1:3]
             else:
                 suggested_name = "Summer 20" + code[1:3]
-                suggested_start_date = "06/01/" + code[1:3]
-                suggested_end_date = "08/31/" + code[1:3]
+                suggested_start_date = "06/01/20" + code[1:3]
+                suggested_end_date = "08/31/20" + code[1:3]
         else:
-            suggested_name = None
-            suggested_start_date = None
-            suggested_end_date = None
+            suggested_name = ""
+            suggested_start_date = ""
+            suggested_end_date = ""
 
+        msg_name = f"What is the name for term '{code}' " + suggested_name + "? "
+        msg_start_date = f"What is the start date for term '{code}' (MM/DD/YYYY) " + suggested_start_date + "? "
+        msg_end_date = f"What is the end date for term '{code}' (MM/DD/YYYY) " + suggested_end_date + "? "
+
+        name = input(msg_name)
+        if name == "":
+            name = suggested_name[1:-1]
+        start_date = enter_and_validate_date(suggested_start_date, msg_start_date)
+        end_date = enter_and_validate_date(suggested_end_date, msg_end_date)
+
+        # INSERT term codes into new terms table.
+        try:
+            database.execute("INSERT INTO terms (term_id, name, start_date, end_date) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING;", (code, name, start_date, end_date))
+        except Exception as e:
+            raise SystemExit("Error INSERTing values for term '{code}'.\n" + str(e))
 
     # Create FK, courses table (semester) references terms table (term_id)
     try:
         database.execute("ALTER TABLE ONLY courses ADD CONSTRAINT courses_fkey FOREIGN KEY (semester) REFERENCES terms (term_id) ON UPDATE CASACADE;")
-    except EXCEPTION as e:
+    except Exception as e:
         raise SystemExit("Error creating FK: courses(semester) references terms(term_id)\n" + str(e))
+# END function up()
 
 def down(config, database):
     """
@@ -76,3 +115,5 @@ def down(config, database):
     :type database: migrator.db.Database
     """
     pass
+
+# EOF
